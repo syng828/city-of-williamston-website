44cc6395493c38f069631e25983882c25b685938
Jasleen Narang <jasleen.narangs@gmail.com>
2023-11-26 17:15:33 -0800

    nav.js

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,33 +2,51 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
-import WilliamstonLogo from "../WilliamstonLogo.jpg"
+import newLogo from "../newLogo.png" 
 
+
+import './Navigation.css';
 //this is the homepage
-const Navigation = () => {  
-    let {user, logoutUser} = useContext(AuthContext)
-    return (
-        <div className= "navbar">
-            <img src = {WilliamstonLogo} alt = "williamston logo"></img>
-            <br></br>
-            <Link to = '/'><button>Home</button></Link> {/*buttons in wrong place*/}
-            <Link to ='/department'><button>Departments</button></Link>
-            <div className = "dropdown">
-                <button className = "dropbtn">Permits</button> 
-                    <div className="dropdown-content">
-                        <Link to = '/permit'>View My Permits</Link>
-                        <Link to = '/permitSend'>Send a New Permit Request</Link>
-                    </div>
-            </div>
-            <Link to = '/contact'><button>Contact Us</button></Link>
+const Navigation = () => { 
+  
+   let {user, logoutUser} = useContext(AuthContext)
+   return (
 
-              {user ? (     //move these buttons to the top right corner later, the rest just in a nav bar
-                <button onClick = {logoutUser}>Logout</button>
-            ) : ( 
-                <Link to = '/login'><button>Login/Sign Up</button></Link>
-            )} 
+
+       <div className= "navbar">
+          
+           <Link to = '/'><img src = {newLogo} alt = "williamston logo" class = "sized"></img></Link>
+         <div class = "links"> 
+           <Link to ='/department' class = "underline first"><button>Departments</button></Link>
+   
+        
+           <div className = "dropdown">
+             <button className = "dropbtn">Permits
+
+
+                   <div className="dropdown-content">
+                       <Link to = '/permit'>View My Permits</Link>
+                       <Link to = '/permitSend'>Send a New Permit Request</Link>
+                   </div>
             
-        </div>
-    )
+              </button> 
+          
+           </div>
+        
+        
+         <Link to = '/contact' class = "underline sec"><button>Contact Us</button></Link>
+
+
+             {user ? (     //move these buttons to the top right corner later, the rest just in a nav bar
+               <button onClick = {logoutUser}>Logout</button>
+           ) : (
+          
+               <Link to = '/login' class = "underline third "><button>Login/Sign Up</button></Link>
+           )}
+           
+       </div>
+       <hr></hr>
+    </div>
+   )
 }
 export default Navigation;
