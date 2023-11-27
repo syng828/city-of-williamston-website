47e763ab83d046af7012f631a1f514cf10f3e4e6
Jasleen Narang <jasleen.narangs@gmail.com>
2023-11-26 16:58:12 -0800

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
+
 function App() {
   return (
     <div className="App">
+      
+      
+     
       <BrowserRouter>
       <AuthProvider>
         <Routes> 
